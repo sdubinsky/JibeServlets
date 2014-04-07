93af78aae07811ed26d4bc2b9610a7a9fd760f54
Scott Dubinsky <smdubinsky@gmail.com>
2014-04-07 17:39:17 -0400

    Allows reading from output.txt file and outputting as json.

--- Jibe Servlets/src/HelloWorld.java
+++ Jibe Servlets/src/HelloWorld.java
@@ -1,10 +1,13 @@
 
 import java.io.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.*;
 
 /**
  * Servlet implementation class HelloWorld
@@ -24,10 +27,33 @@ public class HelloWorld extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
+		response.setContentType("application/json");
+		JSONObject json = new JSONObject();
+		try {
+			json.put("text", "value");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		InputStream input = getServletContext().getResourceAsStream("/WEB-INF/output.txt");
 		
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+		int i = 0;
+		String text = "haha";
+		while ((text = reader.readLine()) != null){
+			try {
+				json.put(Integer.toString(i), text);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			i++;
+		}
 		PrintWriter out = response.getWriter();
-		out.println("<h1>Hello World!</h1>");
+		out.println(json.toString());
+		out.flush();
 	}
 
 	/**
